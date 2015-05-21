f3e37fc5f018c7c3133fc31ec4e2911cbd304efe
mlloewen <mlloewen@gmail.com>
2015-05-20 22:38:11 -0600

    Correct Github link

--- sermons.php
+++ sermons.php
@@ -9,7 +9,7 @@ Author URI: http://www.wpforchurch.com/
 License: GPL2
 Text Domain: sermon-manager
 Domain Path: /languages/
-GitHub Plugin URI: https://github.com/mlloewen/muirlake-theme
+GitHub Plugin URI: https://github.com/mlloewen/sermon-manager-for-muir
 */
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
